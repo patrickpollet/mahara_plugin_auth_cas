2a6eed074ebf113eb5ff28611718f5a71fd58798
patrickpollet <pp@patrickpollet.net>
2012-01-19 14:47:51 +0100

    fixed issue with phpcas logout depending of cas version  see  http://tracker.moodle.org/browse/MDL-27610   and https://wiki.jasig.org/display/CASC/phpCAS+logout

--- lib.php
+++ lib.php
@@ -44,19 +44,19 @@
  *             http://www.gnu.org/copyleft/gpl.html
  */
 
-defined ('INTERNAL') || die();
-require_once(get_config ('docroot') . 'auth/lib.php');
-require_once(get_config ('docroot') . 'auth/ldap/lib.php');
-require_once(get_config ('docroot') . 'auth/cas/CAS/CAS.php');
-require_once(get_config ('docroot') . 'auth/cas/PluginAuthCas.class.php');
+defined('INTERNAL') || die();
+require_once(get_config('docroot') . 'auth/lib.php');
+require_once(get_config('docroot') . 'auth/ldap/lib.php');
+require_once(get_config('docroot') . 'auth/cas/CAS/CAS.php');
+require_once(get_config('docroot') . 'auth/cas/PluginAuthCas.class.php');
 
 /**
  * Authenticates users with CAS and an associated Lightweight Directory Access Protocol
  */
 class AuthCas extends AuthLdap {
 
-    public function __construct ($id = null) {
-        parent::__construct ($id); //takes care of initing the config values if $id <>null
+    public function __construct($id = null) {
+        parent::__construct($id); //takes care of initing the config values if $id <>null
         $this->type = 'cas';
         $this->has_instance_config = true;
         //$this->config['studentidfield2'] = 'supannEmpId'; INSA specific setting
@@ -64,8 +64,8 @@ class AuthCas extends AuthLdap {
         return true;
     }
 
-    public function init ($id = null) {
-        $this->ready = parent::init ($id);
+    public function init($id = null) {
+        $this->ready = parent::init($id);
         // Check that required fields are set
         if (empty($this->config['cas_hostname']) ||
             empty($this->config['cas_baseuri']) ||
@@ -87,20 +87,20 @@ class AuthCas extends AuthLdap {
      *                         authenticated successfully
      * @throws AuthUnknownUserException is no LDAP support
      */
-    public function authenticate_user_account ($user, $password) {
+    public function authenticate_user_account($user, $password) {
 
         // first make sure we are called from auth/cas/index.php
         // this may happen if CAS user typed its credentials in some Mahara login box ...
         global $PHPCAS_CLIENT, $CFG;
-        if (!is_object ($PHPCAS_CLIENT)) {
+        if (!is_object($PHPCAS_CLIENT)) {
             return false;
         }
 
-        $this->must_be_ready ();
+        $this->must_be_ready();
         $username = $user->username;
 
         // check ldap functionality exists
-        if (!function_exists ('ldap_bind')) {
+        if (!function_exists('ldap_bind')) {
             throw new AuthUnknownUserException('LDAP is not available in your PHP environment. Check that it is properly installed');
         }
 
@@ -120,17 +120,17 @@ class AuthCas extends AuthLdap {
         so phpCAS::isAuthenticated fails ...
         $this->connectCAS();
         if (!(phpCAS::isAuthenticated() || (strtolower(phpCAS::getUser()) != $username) )) {
-            pp_error_log("raté ","isAuthenticated");
+        pp_error_log("raté ","isAuthenticated");
         return false;
         }
-        *********************/
+         *********************/
         /*
          * note that if phpCAS::isAuthenticated() has not been called within the same session
          * (only in auth/cas/index.php) before this phpCAS:getUser()
          * this call will die with phpCAS fatal error , so no way to break in ;-)
          * and we do not call connectCAS() either ! this should have been done already in auth/cas/index.php
         */
-        if (strtolower (phpCAS::getUser ()) != $username) {
+        if (strtolower(phpCAS::getUser()) != $username) {
             //pp_error_log("raté ","test getuser");
             return false;
         }
@@ -139,19 +139,19 @@ class AuthCas extends AuthLdap {
         if ($user->id && $update) {
 
             // Retrieve information of user from LDAP via its public method
-            $ldapdetails = $this->get_user_info ($username);
+            $ldapdetails = $this->get_user_info($username);
             // this method returns an object and we want an array below
             $ldapdetails = (array)$ldapdetails;
             // Match database and ldap entries and update in database if required
             $fieldstoimport = array('firstname', 'lastname', 'email', 'studentid', 'preferredname');
             foreach ($fieldstoimport as $field) {
                 $sanitizer = "sanitize_$field";
-                if (function_exists ($sanitizer)) {
+                if (function_exists($sanitizer)) {
                     $ldapdetails[$field] = $sanitizer($ldapdetails[$field]);
                 }
                 if (!empty($ldapdetails[$field]) && ($user->$field != $ldapdetails[$field])) {
                     $user->$field = $ldapdetails[$field];
-                    set_profile_field ($user->id, $field, $ldapdetails[$field]);
+                    set_profile_field($user->id, $field, $ldapdetails[$field]);
                 }
             }
             //pp_error_log ('maj compte ',$ldapdetails);
@@ -160,8 +160,8 @@ class AuthCas extends AuthLdap {
             // these attributes were set in the parent class by a call to init($id)
             // $this->instanceid
             //  $this->institution
-            if (!empty($this->instanceid) && !empty( $this->institution )) {
-                set_field ('usr_institution','studentid',$user->studentid,'usr',$user->id,'institution', $this->institution );
+            if (!empty($this->instanceid) && !empty($this->institution)) {
+                set_field('usr_institution', 'studentid', $user->studentid, 'usr', $user->id, 'institution', $this->institution);
                 //pp_error_log ('maj compte II',$user);
             }
 
@@ -174,29 +174,29 @@ class AuthCas extends AuthLdap {
      * Connect to the CAS (clientcas connection or proxycas connection)
      * borrowed from Moodle code
      */
-    public function connectCAS () {
+    public function connectCAS() {
         global $PHPCAS_CLIENT, $CFG;
 
 
         //  pp_error_log("cas config",$this->config);
         // pp_error_log("cas client",$PHPCAS_CLIENT);
-        if (!is_object ($PHPCAS_CLIENT)) {
+        if (!is_object($PHPCAS_CLIENT)) {
             // Make sure phpCAS doesn't try to start a new PHP session when connecting to the CAS server (false)
             if ($this->config['cas_proxy']) {
-                phpCAS::proxy ((string)$this->config['cas_version'], $this->config['cas_hostname'],
+                phpCAS::proxy((string)$this->config['cas_version'], $this->config['cas_hostname'],
                     (int)$this->config['cas_port'], $this->config['cas_baseuri'], false);
             } else {
-                phpCAS::client ((string)$this->config['cas_version'], $this->config['cas_hostname'],
+                phpCAS::client((string)$this->config['cas_version'], $this->config['cas_hostname'],
                     (int)$this->config['cas_port'], $this->config['cas_baseuri'], false);
             }
 
             if ($this->config['cas_certificatecheck'] && $this->config['cas_certificatepath']) {
-                phpCAS::setCasServerCACert ($this->config['cas_certificatepath']);
+                phpCAS::setCasServerCACert($this->config['cas_certificatepath']);
             } else {
                 // Don't try to validate the server SSL credentials
-                phpCAS::setNoCasServerValidation ();
+                phpCAS::setNoCasServerValidation();
             }
-            phpCAS::setLang ($this->config['cas_language']);
+            phpCAS::setLang($this->config['cas_language']);
         }
     }
 
@@ -204,12 +204,23 @@ class AuthCas extends AuthLdap {
      * @override
      * also logout from CAS is specified in the configuration
      */
-    public function logout () {
+    public function logout() {
         global $CFG;
         if ($this->config['cas_logout']) {
             $backurl = $CFG->wwwroot;
-            $this->connectCAS ();
-            phpCAS::logoutWithURL ($backurl);
+            $this->connectCAS();
+            // phpCAS::logoutWithURL ($backurl);
+            //should be with CAS server >=3.3.5 see  http://tracker.moodle.org/browse/MDL-27610   and https://wiki.jasig.org/display/CASC/phpCAS+logout
+            //phpCAS::logoutWithRedirectService($backurl);
+
+            if (method_exists('phpCAS', 'logoutWithRedirectService'))    {
+                //pp_error_log ('logout via','phpCAS::logoutWithRedirectService');
+                phpCAS::logoutWithRedirectService($backurl);
+            }
+            else {
+                //pp_error_log('logout via ','phpCAS::logoutWithURL');
+                phpCAS::logoutWithURL($backurl);
+            }
         }
     }
 
